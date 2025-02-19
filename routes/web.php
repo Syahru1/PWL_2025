dbc10bc96d0f888e5145ecec3cd4f3aec2e60684
Muhammad Syahrul Gunawan <syahrulgunawann41@gmail.com>
2025-02-19 09:55:53 +0700

    pertemuan kedua

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,46 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/hello', function () {
+    return 'Hello World'; });
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/p', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'NIM: 2341720002 <br> Nama: Muhammad Syahrul Gunawan';
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user/{name}    ', function ($name) {    
+    return 'Nama saya '.$name; 
+});
+
+Route::get('/posts/{post}/comments/{comments}', function($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articless/{id}', function ($id) {
+    return "Halaman Artikel dengan ID $id";
+});
+
+Route::get('/user/{name?}', function($name = 'Guest') {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/{name?}', function($name = 'John') {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/profile', function() {
+    //
+})->name('profile');
+
